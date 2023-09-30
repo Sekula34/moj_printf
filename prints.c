104089666d67b1846a5c169ea35cb763ebbc4ea2
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-09-30 18:29:27 +0200

    norma

--- prints.c
+++ prints.c
@@ -18,7 +18,7 @@ long	prints(va_list lista)
 	long	string_length;
 
 	string = va_arg(lista, char *);
-	if(string == NULL)
+	if (string == NULL)
 		return (0);
 	ft_putstr_fd(string, 1);
 	string_length = ft_strlen(string);
